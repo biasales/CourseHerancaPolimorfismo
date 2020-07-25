43303f32413f14e3626e98f652662b63f81b9ed5
Bianca Sales <salesbyankaa@gmail.com>
2020-07-25 16:34:34 -0300

    isto eh um commit

---
+++ Entites/OutsourcedEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course2.Entites
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdittionalCharge { get; set; }
+
+        public OutsourcedEmployee()
+        {
+
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour,double adittionalCharge) : base(name,hours,valuePerHour)
+        {
+            AdittionalCharge = adittionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdittionalCharge;
+
+        }
+    }
+}
